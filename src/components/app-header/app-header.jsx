60d3fa9339f3da503c1c01e7895e1fcafe36584b
Stepan Popov <bimimot@yandex.ru>
2021-08-12 11:25:09 +0300

    Fix structure

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default class AppHeader extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {}        
+    }
+
+    render() {
+        return(
+            <h1>HEADER</h1>
+        )
+    }
+
+}
