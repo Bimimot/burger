2b8aa89cedf98d1c8e908afa98b9210b64659c4b
Stepan Popov <bimimot@yandex.ru>
2021-08-25 16:19:37 +0300

    Set error-boundary

---
+++ src/components/error-boundary/error-boundary.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
+import { ErrorMessage } from '../error-message/error-message';
+
+export default class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, info) {
+        console.log('Cath error', error, info);
+    }
+
+    render() {
+        return (
+            this.state.hasError
+                ? <ErrorMessage />
+                : this.props.children
+        )
+    }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {App} from './components/app/app';
+import { App } from './components/app/app';
+import ErrorBoundary from './components/error-boundary/error-boundary';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
